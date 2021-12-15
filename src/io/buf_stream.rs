03803503875625d49c1b7f7e2a19f972911a7112
zhuxiujia@qq.com <zhuxiujia@qq.com>
2021-12-15 20:02:54 +0800

    fix

--- src/io/buf_stream.rs
+++ src/io/buf_stream.rs
@@ -1,11 +1,11 @@
 use bytes::BytesMut;
 use std::io::Cursor;
+use may::net::TcpStream;
+use crate::io::encode::Encode;
+use crate::io::write_and_flush::WriteAndFlush;
 
-pub struct BufStream<S>
-    where
-        S: Unpin,
-{
-    pub(crate) stream: S,
+pub struct BufStream {
+    pub(crate) stream: TcpStream,
 
     // writes with `write` to the underlying stream are buffered
     // this can be flushed with `flush`
@@ -16,34 +16,31 @@ pub struct BufStream<S>
 }
 
 
-impl<S> BufStream<S> where
-    S: Unpin,
+impl BufStream where
 {
-    pub fn new(stream: S) -> Self {
+    pub fn new(stream: TcpStream) -> Self {
         Self {
             stream,
             wbuf: Vec::with_capacity(512),
             rbuf: BytesMut::with_capacity(4096),
         }
     }
-    // pub fn write<'en, T>(&mut self, value: T)
-    //     where
-    //         T: Encode<'en, ()>,
-    // {
-    //     self.write_with(value, ())
-    // }
-    // pub fn write_with<'en, T, C>(&mut self, value: T, context: C)
-    //     where
-    //         T: Encode<'en, C>,
-    // {
-    //     value.encode_with(&mut self.wbuf, context);
-    // }
-    // pub fn flush(&mut self) -> WriteAndFlush<'_, S> {
-    //     WriteAndFlush {
-    //         stream: &mut self.stream,
-    //         buf: Cursor::new(&mut self.wbuf),
-    //     }
-    // }
-
-
+    pub fn write<'en, T>(&mut self, value: T)
+        where
+            T: Encode<'en, ()>,
+    {
+        self.write_with(value, ())
+    }
+    pub fn write_with<'en, T, C>(&mut self, value: T, context: C)
+        where
+            T: Encode<'en, C>,
+    {
+        value.encode_with(&mut self.wbuf, context);
+    }
+    pub fn flush(&mut self) -> WriteAndFlush<'_> {
+        WriteAndFlush {
+            stream: &mut self.stream,
+            buf: Cursor::new(&mut self.wbuf),
+        }
+    }
 }
