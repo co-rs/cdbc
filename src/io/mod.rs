3c23ce77fa62ae75fa9b90fd8da3a02489ddee25
zhuxiujia <zhuxiujia@qq.com>
2021-12-15 15:26:08 +0800

    impl io

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -1,3 +1,6 @@
 pub mod buf;
 pub mod buf_mut;
 pub mod buf_stream;
+pub mod decode;
+pub mod encode;
+pub mod write_and_flush;
---
+++ src/io/write_and_flush.rs
@@ -0,0 +1,34 @@
+use std::io::{BufRead, Cursor, Write};
+use crate::error::Error;
+use may::net::TcpStream;
+
+// Atomic operation that writes the full buffer to the stream, flushes the stream, and then
+// clears the buffer (even if either of the two previous operations failed).
+pub struct WriteAndFlush<'a> {
+    pub(super) stream: &'a mut TcpStream,
+    pub(super) buf: Cursor<&'a mut Vec<u8>>,
+}
+
+impl <'a>WriteAndFlush<'a> {
+    pub fn flush(mut self)->crate::error::Result<()>{
+        loop {
+            let read = self.buf.fill_buf()?;
+            if !read.is_empty() {
+                let written = self.stream.write(read)?;
+                self.buf.consume(written);
+            } else {
+                break;
+            }
+        }
+        return Ok(());
+    }
+}
+
+impl<'a> Drop for WriteAndFlush<'a> {
+    fn drop(&mut self) {
+        // clear the buffer regardless of whether the flush succeeded or not
+        self.buf.get_mut().clear();
+    }
+}
+
+
