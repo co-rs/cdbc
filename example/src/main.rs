584a75f6ac79deb3931ab04c36240cda0177dfac
zhuxiujia@qq.com <zhuxiujia@qq.com>
2021-12-17 23:40:45 +0800

    add stream

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,27 +1,4 @@
-use std::mem;
 
 fn main() {
-    let mut empty=vec![];
-
-
-   let mut i=0;
-   let mut vec = &mut vec![];
-    let mut items = &mut vec;
-   let v={
-       loop {
-          match poll(&mut i) {
-               Some(x) => items.extend(Some(x)),
-               None => break mem::replace(items, &mut empty),
-          }
-       }
-   };
-    println!("{:?}",v);
-}
-
-fn poll(i:&mut i32)->Option<i32>{
-    *i+=1;
-    if *i==4{
-        return None;
-    }
-    return Some(2);
+    println!("Hello, world!");
 }
