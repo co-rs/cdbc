c408c34baca0d5b08cfec8300ac896d38aac5a44
zhuxiujia <zhuxiujia@qq.com>
2021-12-20 16:40:41 +0800

    change ssl-mode to disable

--- example/src/http/main.rs
+++ example/src/http/main.rs
@@ -64,7 +64,7 @@ impl HttpService for HelloWorld {
 // start the server in main
 fn main() {
     ///if use ssl,or debug. Release mode doesn't require that much stack memory
-    may::config().set_stack_size(0x2000);
+    may::config().set_stack_size(8*1024);//8kb
     let server = HttpServer(HelloWorld).start("0.0.0.0:8000").unwrap();
     println!("http start on http://127.0.0.1:8000");
     server.join().unwrap();
