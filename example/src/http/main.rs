cf01eb149295f3fcadd873ba5d02abb470d16084
zhuxiujia <zhuxiujia@qq.com>
2021-12-20 14:32:11 +0800

    add http example

--- example/src/http/main.rs
+++ example/src/http/main.rs
@@ -58,6 +58,6 @@ impl HttpService for HelloWorld {
 fn main() {
     may::config().set_stack_size(0x2000);
     let server = HttpServer(HelloWorld).start("0.0.0.0:8000").unwrap();
-    println!("http start on http://localhost:8000");
+    println!("http start on http://127.0.0.1:8000");
     server.join().unwrap();
 }
