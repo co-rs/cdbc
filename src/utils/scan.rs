80c3ac58b1cfe7d150b3cd75ad2e5ea20688dd23
zhuxiujia@qq.com <zhuxiujia@qq.com>
2021-12-20 20:15:24 +0800

    add table

--- src/utils/scan.rs
+++ src/utils/scan.rs
@@ -1,3 +1,17 @@
+
+/// scan CDBC Row to an Table,return cdbc::Result<Table>
+/// for example:
+///
+///  pub struct BizActivity {
+///     pub id: Option<String>,
+///     pub name: Option<String>,
+///     pub delete_flag: Option<i32>,
+///   }
+///
+///    let mut data = conn.fetch_all("select * from biz_activity;")?;
+///    for row in data {
+///       let biz_activity = cdbc::scan_row_struct!(x,BizActivity{id: None,name: None,delete_flag: None})
+///    }
 #[macro_export]
 macro_rules! scan_row_struct {
     ($row:ident,$target:path{$($field_name:ident: $field_value:expr$(,)?)+}) => {
