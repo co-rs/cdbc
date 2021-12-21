422a7816f569573ff5c99733d42e281a0fae1379
zhuxiujia <zhuxiujia@qq.com>
2021-12-21 14:05:28 +0800

    add doc

--- src/utils/scan.rs
+++ src/utils/scan.rs
@@ -8,9 +8,14 @@
 ///     pub delete_flag: Option<i32>,
 ///   }
 ///
+///   // fetch one row
+///    let mut row = conn.fetch_one("select * from biz_activity limit 1")?;
+///    let biz_activity = cdbc::row_scan_struct!(row,BizActivity{id: None,name: None,delete_flag: None})
+///
+///    //fetch row vec
 ///    let mut data = conn.fetch_all("select * from biz_activity;")?;
 ///    for row in data {
-///       let biz_activity = cdbc::row_scan_struct!(x,BizActivity{id: None,name: None,delete_flag: None})
+///       let item = cdbc::row_scan_struct!(x,BizActivity{id: None,name: None,delete_flag: None})
 ///    }
 #[macro_export]
 macro_rules! row_scan_struct {
@@ -39,6 +44,17 @@ macro_rules! row_scan_struct {
     }
 }
 
+/// scan CDBC Row to Table,return cdbc::Result<Vec<Table>>
+/// for example:
+///
+///  pub struct BizActivity {
+///     pub id: Option<String>,
+///     pub name: Option<String>,
+///     pub delete_flag: Option<i32>,
+///   }
+///
+///    let mut rows = conn.fetch_all("select * from biz_activity;")?;
+///    let biz_activitys = cdbc::row_scan_structs!(rows,BizActivity{id: None,name: None,delete_flag: None})
 #[macro_export]
 macro_rules! row_scan_structs {
    ($rows:ident,$target:path{$($field_name:ident: $field_value:expr$(,)?)+}) => {
