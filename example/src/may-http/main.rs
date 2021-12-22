492cf2bc23e68802bacdb0387214f0913bd92098
zhuxiujia@qq.com <zhuxiujia@qq.com>
2021-12-22 23:14:22 +0800

    add test

--- example/src/may-http/main.rs
+++ example/src/may-http/main.rs
@@ -1,14 +1,17 @@
 extern crate may_minihttp;
 #[macro_use]
 extern crate lazy_static;
+
+use std::fs::File;
 use std::io;
 use may_minihttp::{HttpServer, HttpService, Request, Response};
 use cdbc::executor::Executor;
 use cdbc::pool::Pool;
-use cdbc_mysql::{MySql, MySqlPool};
+use cdbc::PoolOptions;
+use cdbc_sqlite::{Sqlite, SqlitePool};
 
 lazy_static!(
-    pub static ref POOL: Pool<MySql> = MySqlPool::connect("mysql://root:123456@localhost:3306/test").unwrap();
+    pub static ref POOL: Pool<Sqlite> = make_sqlite_pool().unwrap();
 );
 
 // implement the `HttpService` trait for your service
@@ -66,4 +69,16 @@ fn main() {
     let server = HttpServer(HelloWorld).start("0.0.0.0:8000").unwrap();
     println!("http start on http://127.0.0.1:8000");
     server.join().unwrap();
+}
+
+fn make_sqlite_pool() -> cdbc::Result<SqlitePool> {
+    //first. create sqlite dir/file
+    std::fs::create_dir_all("target/db/");
+    File::create("target/db/sqlite.db");
+    //Concurrent reads and writes to SQLite limit set connection number to 1
+    let pool = PoolOptions::<Sqlite>::new().max_connections(1).connect("sqlite://target/db/sqlite.db")?;
+    let mut conn = pool.acquire()?;
+    conn.execute("CREATE TABLE biz_activity(  id string, name string,age int, delete_flag int) ");
+    conn.execute("INSERT INTO biz_activity (id,name,age,delete_flag) values (\"1\",\"1\",1,0)");
+    Ok(pool)
 }
