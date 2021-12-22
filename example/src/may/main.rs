8f578d0eadc64add2639f426d45133bdb060ea73
zhuxiujia <zhuxiujia@qq.com>
2021-12-22 15:23:11 +0800

    add may test

---
+++ example/src/may/main.rs
@@ -0,0 +1,43 @@
+use std::fs::File;
+use may::coroutine::sleep;
+use std::time::Duration;
+use may::go;
+use cdbc::Executor;
+use cdbc_sqlite::SqlitePool;
+
+fn main(){
+    //spawn coroutines
+    go!(||{
+      run_sqlite();
+    });
+    sleep(Duration::from_secs(3));
+}
+
+fn run_sqlite() -> cdbc::Result<()> {
+    let pool = make_sqlite()?;
+    #[derive(Debug)]
+    pub struct BizActivity {
+        pub id: Option<String>,
+        pub name: Option<String>,
+        pub delete_flag: Option<i32>,
+    }
+    let data = cdbc::row_scans!(
+        cdbc::query("select * from biz_activity where id = ?")
+        .bind("1")
+        .fetch_all(pool.clone())?,
+        BizActivity{id:None,name:None,delete_flag:None})?;
+    println!("{:?}", data);
+    Ok(())
+}
+
+fn make_sqlite() -> cdbc::Result<SqlitePool> {
+    //first. create sqlite dir/file
+    std::fs::create_dir_all("target/db/");
+    File::create("target/db/sqlite.db");
+    //next create table and query result
+    let pool = SqlitePool::connect("sqlite://target/db/sqlite.db")?;
+    let mut conn = pool.acquire()?;
+    conn.execute("CREATE TABLE biz_activity(  id string, name string,age int, delete_flag int) ");
+    conn.execute("INSERT INTO biz_activity (id,name,age,delete_flag) values (\"1\",\"1\",1,0)");
+    Ok(pool)
+}
