22ef9f15ab308bf9ba8c3364322c31f07c5365c1
zhuxiujie <zhuxiujia@qq.com>
2022-02-16 15:03:23 +0800

    add mssql code

---
+++ example/src/mssql/main-mssql.rs
@@ -0,0 +1,24 @@
+use cogo::std::time::Time;
+
+fn main() -> cdbc::Result<()> {
+    #[derive(Debug,serde::Serialize)]
+    pub struct BizActivity {
+        pub id: Option<String>,
+        pub name: Option<String>,
+        pub delete_flag: Option<i32>,
+        pub create_time: Option<Time>
+    }
+
+    Ok(())
+}
+
+
+#[cfg(test)]
+mod test {
+
+    #[test]
+    fn test_stream_mysql() -> cdbc::Result<()> {
+
+        Ok(())
+    }
+}
